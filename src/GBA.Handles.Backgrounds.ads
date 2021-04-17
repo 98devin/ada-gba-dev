fc5d0b86ea73e86d5081ae8f23ec16f0648f02a3
Devin Hill <98devin@gmail.com>
2021-04-17 14:41:20 -0400

    begin bg handle interface; need some numeric utilities first.

---
+++ src/GBA.Handles.Backgrounds.ads
@@ -0,0 +1,27 @@
+
+with GBA.Display.Backgrounds;
+use  GBA.Display.Backgrounds;
+
+with Interfaces;
+use  Interfaces;
+
+
+package GBA.Handles.Backgrounds is
+
+  type BG_Ref is interface;
+
+  function Kind (BG : BG_Ref) return BG_Kind is abstract;
+
+  function Vertical_Offset (BG : BG_Ref) return
+
+  procedure Set_Vertical_Offset   (BG : BG_Ref; Offset : BG_Scroll_Offset) is abstract;
+  procedure Set_Horizontal_Offset (BG : BG_Ref; Offset : BG_Scroll_Offset) is abstract;
+
+  procedure Set_Offsets (BG : BG_Ref; Offsets : BG_Offset_Info) is abstract;
+  procedure Set_Offsets (BG : BG_Ref; Horizontal, Vertical : BG_Scroll_Offset) is abstract;
+
+  procedure Shift_Vertical   (BG : BG_Ref; Offset : Integer_16) is abstract;
+  procedure Shift_Horizontal (BG : BG_Ref; Offset : Integer_16) is abstract;
+  procedure Shift (BG : BG_Ref; Vertical, Horizontal : Integer_16 := 0) is abstract;
+
+end GBA.Handles.Backgrounds;
